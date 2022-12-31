de502675c448c29c7f58539c2724e3e73e98483a
Inna Zorina <innazorina0101@gmail.com>
2022-12-31 20:29:01 +0200

    робить вправи

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// const apples = 47;
+// const grapes = 136;
+// const total = apples + grapes;
+// console.log(total);
+// const diff = apples - grapes;
+// console.log(diff);
+
+// let students = 100;
+// students += 50;
+// console.log(students);
+
+// const result = 108 + 223 - 2 * 5;
+// console.log(result);
+
+    // Math
+
+// const value = 27.9;
+// console.log(Math.floor(value));
+// console.log(Math.ceil(value));
+// console.log(Math.round(value));
+
+// Шаблонні рядки
+
+const companyName = "Cyberdyne Systems";
+const repairBots = 100;
+const defenceBots = 50;
+const message = `Cyberdyne Systems has ${repairBots + defenceBots} bots in stock`;
+console.log(message);
+
